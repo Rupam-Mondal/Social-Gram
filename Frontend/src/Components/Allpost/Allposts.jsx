0ad314d915f48b7e37795ff6c0c17d47d36122f8
Rupam <rupammondal564@gmail.com>
2024-10-18 10:29:50 +0530

    Infinite scrolling added

--- Frontend/src/Components/Allpost/Allposts.jsx
+++ Frontend/src/Components/Allpost/Allposts.jsx
@@ -2,28 +2,53 @@ import { getAllImage } from "../../Services/Allimagefetch";
 import { useQuery } from "react-query";
 import MiniPostCreationSection from "../miniPostCreation/miniPostCreation";
 import Imagecard from "../ImageCard/Imagecard";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useEffect, useState } from "react";
 
-function Allposts(){
+function Allposts() {
+    const [offset, setOffset] = useState(0);
+    const [dataarray, setDataarray] = useState([]);
+    const [postnumber, setPostnumber] = useState(5);
 
-    const { data, error, isLoading } = useQuery(['images'] , () => getAllImage(5 , 0));
+    const { data, error, isLoading } = useQuery(['images', offset], () => getAllImage(postnumber, offset), {
+        keepPreviousData: true,
+    });
+
+    useEffect(() => {
+        if (data) {
+            setDataarray((prev) => [...prev, ...data.data.allpost]);
+        }
+    }, [data]);
+
+    function fetchmoredata() {
+        setOffset(offset + postnumber);
+    }
 
     return (
         <>
-            <div className="h-full w-2/4 bg-black border-l-[1px] border-white overflow-y-auto">
-                {/* post section */}
-                <MiniPostCreationSection/>
-                {
-                    data && data.data.allpost.map((v , i) => (
-                        <Imagecard
-                            key={i}
-                            caption={v.caption}
-                            Image={v.image}
-                        />
-                    ))
-                }
+            <div id="scrollableDiv" className="h-full w-2/4 border-l-[1px] border-white overflow-y-auto">
+                <InfiniteScroll
+                    dataLength={dataarray.length}
+                    hasMore={true}
+                    loader={<h4>Loading...</h4>}
+                    next={fetchmoredata}
+                    scrollableTarget="scrollableDiv" // Bind to this specific div
+                >
+                    <MiniPostCreationSection />
+                    {
+                        dataarray && dataarray.map((v, i) => (
+                            <Imagecard
+                                key={i}
+                                caption={v.caption}
+                                Image={v.image}
+                            />
+                        ))
+                    }
+                </InfiniteScroll>
             </div>
         </>
-    )
+    );
 }
+
 
 export default Allposts;
