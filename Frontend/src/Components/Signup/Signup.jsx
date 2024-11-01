0f2a825f833fbf19441b577408da978dd3ddfd89
Rupam <rupammondal564@gmail.com>
2024-11-01 09:37:07 +0530

    Signup page done

--- Frontend/src/Components/Signup/Signup.jsx
+++ Frontend/src/Components/Signup/Signup.jsx
@@ -1,55 +1,9 @@
-import { useState } from "react";
-import Loginservice from "../../Services/LoginService";
-import { useNavigate } from "react-router";
-
-function Auth() {
-
-    const [username , setUsername] = useState(null);
-    const [password , setPassword] = useState(null);
-    const [email , setEmail] = useState();
-    const navigate = useNavigate();
-
-    async function handleSubmit(e){
-        e.preventDefault();
-        if(username && password){
-            const userLoginObject = {
-                "email":email,
-                "username":username,
-                "password":password
-            };
-
-            const response = await Loginservice(userLoginObject);
-            console.log(response)
-            if(response.success){
-                navigate('/home')
-            }
-        }
-    }
-
-    function signupReDirect(){
-        navigate('/signup')
-    }
-
+function Signup() {
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="bg-white p-8 rounded shadow-md w-96">
-                <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label htmlFor="Email" className="block text-gray-700">
-                            Email
-                        </label>
-                        <input
-                            type="text"
-                            id="Email"
-                            className="w-full p-2 border border-gray-300 rounded mt-1 focus:outline-none focus:border-blue-500"
-                            required
-                            onChange={(e) => {
-                                setEmail(e.target.value);
-                            }}      
-                        />
-                    </div>
-
+                <h2 className="text-2xl font-bold mb-6 text-center">Signup</h2>
+                <form>
                     <div className="mb-4">
                         <label htmlFor="username" className="block text-gray-700">
                             Username
@@ -59,9 +13,18 @@ function Auth() {
                             id="username"
                             className="w-full p-2 border border-gray-300 rounded mt-1 focus:outline-none focus:border-blue-500"
                             required
-                            onChange={(e) => {
-                                setUsername(e.target.value);
-                            }}
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="username" className="block text-gray-700">
+                            Email
+                        </label>
+                        <input
+                            type="text"
+                            id="email"
+                            className="w-full p-2 border border-gray-300 rounded mt-1 focus:outline-none focus:border-blue-500"
+                            required
                         />
                     </div>
                     <div className="mb-6">
@@ -73,18 +36,13 @@ function Auth() {
                             id="password"
                             className="w-full p-2 border border-gray-300 rounded mt-1 focus:outline-none focus:border-blue-500"
                             required
-                            onChange={(e) => {
-                                setPassword(e.target.value);
-                            }}
                         />
                     </div>
-
-                    <div className="text-red-500 mb-5 cursor-pointer" onClick={signupReDirect}>Don't have account?click here</div>
                     <button
                         type="submit"
                         className="w-full bg-blue-500 text-white font-bold py-2 rounded hover:bg-blue-600 transition duration-200"
                     >
-                        Login
+                        Register
                     </button>
                 </form>
             </div>
@@ -92,4 +50,4 @@ function Auth() {
     );
 }
 
-export default Auth;
+export default Signup;
--- Frontend/src/Routes/Routing.jsx
+++ Frontend/src/Routes/Routing.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../Components/Pages/Home";
 import Explore from "../Components/Pages/Explorepage";
 import Auth from "../Components/Login/Login";
+import Signup from "../Components/Signup/Signup";
 
 export function Routing(){
     return (
@@ -9,6 +10,7 @@ export function Routing(){
             <Routes>
                 <Route path="/" element={<Auth/>}></Route>
                 <Route path="/home" element={<Home />}></Route>
+                <Route path="/signup" element={<Signup/>}></Route>
                 <Route path="/Explore" element={<Explore />}></Route>
                 <Route path="/Notification" element={<Explore />}></Route>
                 <Route path="/Messages" element={<Explore />}></Route>
