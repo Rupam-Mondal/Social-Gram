f73ffa8dbd341e488184561b5a9f97c1a1f406ff
Rupam <rupammondal564@gmail.com>
2024-11-01 07:43:49 +0530

    Update

--- Frontend/src/Routes/Routing.jsx
+++ Frontend/src/Routes/Routing.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../Components/Pages/Home";
 import Explore from "../Components/Pages/Explorepage";
-import Auth from "../Components/Signup/Signup";
+import Auth from "../Components/Login/Login";
 
 export function Routing(){
     return (
         <>
             <Routes>
                 <Route path="/" element={<Auth/>}></Route>
+                <Route path="/home" element={<Home />}></Route>
                 <Route path="/Explore" element={<Explore />}></Route>
                 <Route path="/Notification" element={<Explore />}></Route>
                 <Route path="/Messages" element={<Explore />}></Route>
