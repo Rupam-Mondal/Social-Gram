cc1b025874f20e8903eb69f4bfcf68f13afaae44
Rupam <rupammondal564@gmail.com>
2024-11-01 08:37:03 +0530

    Login setup done

--- Frontend/src/Components/Login/Login.jsx
+++ Frontend/src/Components/Login/Login.jsx
@@ -11,7 +11,6 @@ function Auth() {
 
     async function handleSubmit(e){
         e.preventDefault();
-        console.log(username , email , password)
         if(username && password){
             const userLoginObject = {
                 "email":email,
