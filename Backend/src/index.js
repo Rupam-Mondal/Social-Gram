68e3740f0719b7ebedde715d983dc9b54617bb32
Rupam <rupammondal564@gmail.com>
2024-11-15 08:10:38 +0530

    Backend updated

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -24,8 +24,8 @@ app.use(express.text());
 app.use(limiter)
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
-    methods: ['GET', 'POST' , 'DELETE'], 
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE'],
     credentials: true,
 }));
 
